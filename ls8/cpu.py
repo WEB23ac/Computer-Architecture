16a2e55939e7d6ac6b3492a87c65c40d8e2feb24
Aaron Clark <aaronpclark92@gmail.com>
2020-03-26 18:18:51 -0500

    feat: day three mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -106,8 +106,8 @@ class CPU:
         if op == 'MUL':
             self.register[reg_a] *= self.register[reg_b]
         # elif op == "SUB": etc
-        else:
-            raise Exception("Unsupported ALU operation")
+        # else:
+        #     raise Exception("Unsupported ALU operation")
 
     def trace(self):
         """
@@ -137,14 +137,17 @@ class CPU:
 
     def interpret_command(self, opcode):
         # opcode = int(opcode)
-        # print(f'interpreter opcode: {opcode}')
+        print(f'interpreter opcode: {opcode}')
         commands = {
             0b10000010: 'LDI',
             0b01000111: 'PRN',
             0b00000001: 'HLT',
+            0b10100000: 'ADD',
             0b10100010: 'MUL',
             0b01000101: 'PUSH',
-            0b01000110: 'POP'
+            0b01000110: 'POP',
+            0b01010000: 'CALL',
+            0b00010001: 'RET'
         }
         # print('matching command = ', commands[opcode], type(commands[opcode]))
         return commands[opcode]
@@ -156,14 +159,16 @@ class CPU:
 
         while running:
             command = self.ram_read(self.pc)
+            print('command', command)
             ir = self.interpret_command(command)
+            print('ir', ir)
             operand_a = self.ram_read(self.pc+1)
             operand_b = self.ram_read(self.pc+2)
             if ir == 'LDI':
                 self.register[operand_a] = operand_b
                 self.pc += 3
             if ir == 'MUL':
-                self.alu(ir, operand_a, operand_b)
+                self.alu('MUL', operand_a, operand_b)
                 self.pc += 3
             if ir == 'PRN':
                 print(self.register[operand_a])
@@ -179,9 +184,8 @@ class CPU:
                 self.pc += 2
 
             if ir == 'POP':
-                reg = operand_a
                 val = self.ram_read(self.register[self.sp])
-                self.register[reg] = val
+                self.register[operand_a] = val
                 # * Incrememnt value at SP
                 self.register[self.sp] += 1
                 self.pc += 2
@@ -189,6 +193,21 @@ class CPU:
             if ir == 'HLT':
                 running = False
                 self.pc += 1
+
+            if ir == 'ADD':
+                self.alu('ADD', operand_a, operand_b)
+                self.pc += 3
+
+            elif ir == 'CALL':
+                self.register[self.sp] -= 1
+                self.ram[self.register[self.sp]] = self.pc + 2
+                self.pc = self.register[operand_a]
+
+            elif ir == 'RET':
+                val = self.ram[self.register[self.sp]]
+                self.pc = val
+                self.register[self.sp] += 1
+
             # else:
             #     print(f'ir {command} not recognized.')
             #     sys.exit()
